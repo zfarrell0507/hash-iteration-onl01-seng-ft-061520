55ec9fc6a9391e56fa30586d567ddc9ba229e411
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 15:26:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,7 +1,9 @@
 birthday_kids = {"Timmy" => 9, "Sarah" => 6, "Amanda" => 27}
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
