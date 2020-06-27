c9c6bba5988f7441cbcd56aa886d896e033c43ab
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 15:32:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,5 +13,9 @@ def age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
       
     else
-      puts ""
+      puts "You are too old for this."
+    end
+  end
+end
+age_appropriate_birthday(birthday_kids)
 
