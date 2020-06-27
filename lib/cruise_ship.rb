82352c05f5a5bb04778cf72465e85cc8713a8515
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 15:23:44 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,9 +10,10 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = name
+    winner = name
     end
-    passengers
   end
+ 
+  winner
 end
 select_winner(passengers)
